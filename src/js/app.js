bc3a29449e21279d72897e15714a3bbef669a77c
Artemiy Nikonov <artemii.niconov@gmail.com>
2017-08-27 12:08:48 +0300

    Current page

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+// window.onload = function() {
+//   var path = window.location.href
+//   var els = $("dl.topbarsub dd a").map (function(i, e) {
+//     if ($(e)[0] == path) {
+//       $(e).parent().addClass('active')
+//     } else {
+//       $(e).parent().removeClass('active')
+//     };
+//   })
+// }
